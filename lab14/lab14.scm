5137cf918c0cebf1b556352ba0e36b2b60d47597
shejialuo <shejialuo@gmail.com>
2022-05-22 14:43:01 +0800

    Finish Lab 14

--- lab14/lab14.scm
+++ lab14/lab14.scm
@@ -1,5 +1,12 @@
 (define (split-at lst n)
-  'YOUR-CODE-HERE
+  (define (helper former latter lst i)
+    (cond
+      ((null? lst) (cons former latter))
+      ((<= i n) (helper (append  former (list (car lst)) ) latter (cdr lst) (+ i 1) ))
+      (else (helper former (append latter (list (car lst)) ) (cdr lst) (+ i 1) ))
+    )
+  )
+  (helper nil nil lst 1)
 )
 
 
