43b20a6d46066ce4b631efa5f53c714dee61677c
shejialuo <shejialuo@gmail.com>
2022-05-22 13:59:02 +0800

    Finish Lab 13

--- lab13/lab13.sql
+++ lab13/lab13.sql
@@ -2,32 +2,44 @@
 
 
 CREATE TABLE bluedog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet FROM students WHERE color = 'blue' AND pet = 'dog';
 
 CREATE TABLE bluedog_songs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  SELECT color, pet, song FROM students WHERE color = 'blue' AND pet = 'dog';
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT A.pet, A.song, A.color, B.color
+  FROM students as A, students as B
+  WHERE A.pet = B.pet And A.song = B.song AND A.time < B.time;
 
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT students.seven
+  FROM students , numbers
+  WHERE students.time = numbers.time
+        AND students.number = 7 AND numbers.'7' = "True";
 
 
 CREATE TABLE favpets AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, COUNT(pet) AS num
+  FROM students
+  GROUP BY pet ORDER BY num desc LIMIT 10;
 
 
 CREATE TABLE dog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, COUNT(pet)
+  FROM students
+  WHERE pet='dog';
 
 
 CREATE TABLE bluedog_agg AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT song, COUNT(song) as num
+  FROM bluedog_songs
+  GROUP BY song ORDER BY num desc;
 
 
 CREATE TABLE instructor_obedience AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven, instructor, COUNT(*)
+  FROM students
+  WHERE seven='7' GROUP BY instructor;
 
